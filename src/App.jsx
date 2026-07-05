301755db93a82520f22a39a64351ba5ee388990f
Kurtis Pivert <kurtis.pivert@gmail.com>
2026-07-05 12:38:33 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState, useRef } from "react";
+import { useDimensions } from "./use-dimensions.js";
+import "./App.css";
+import { data } from "./data.js";
+
+function App() {
+  const chart1Ref = useRef(null);
+  console.log(data.map((d) => d.country));
+  return <></>;
+}
+
+export default App;
