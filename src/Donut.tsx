31bf31f44ee40ca7b4f80f1981d8c162ce986d40
Kurtis Pivert <kurtis.pivert@gmail.com>
2026-08-01 12:07:43 -0400

    Add legend

--- src/Donut.tsx
+++ src/Donut.tsx
@@ -28,6 +28,9 @@ export const data2024 = Object.entries(world2024[0])
   })) as DataItem[];
 
 const MARGIN = 30;
+const MARGIN_X = 150;
+const MARGIN_Y = 50;
+const INFLEXION_PADDING = 20;
 
 const colors = [
   "#4269d0",
@@ -53,8 +56,6 @@ export const ResponsiveDonut = ({
     return null;
   }
 
-  console.log(data2024);
-
   return (
     <div ref={chartRef} style={{ width: "100%", height: "100%" }}>
       <Donut
@@ -67,34 +68,106 @@ export const ResponsiveDonut = ({
 };
 
 const Donut = ({ data2024, width, height }: ResponsiveDonutProps) => {
-  const radius = Math.min(width, height) / 2 - MARGIN;
+  const radius = Math.min(width - 2 * MARGIN_X, height - 2 * MARGIN_Y) / 2;
   const pieGenerator = d3.pie<any, DataItem>().value((d) => d.value);
 
   const pie = useMemo(() => pieGenerator(data2024), [data2024]);
 
+  // console.log(pie);
+
   const arcPathGenerator = d3.arc();
 
-  const arcs = pie.map(
-    (p) =>
-      arcPathGenerator({
-        innerRadius: 0,
-        outerRadius: radius,
-        startAngle: p.startAngle,
-        endAngle: p.endAngle,
-      }) ?? undefined,
-  );
+  const shapes = pie.map((grp, i) => {
+    // First Arc is for the Pie...then donut
+
+    const sliceInfo = {
+      innerRadius: 0,
+      outerRadius: radius,
+      startAngle: grp.startAngle,
+      endAngle: grp.endAngle,
+    };
+
+    const centroid = arcPathGenerator.centroid(sliceInfo);
+    const slicePath = arcPathGenerator(sliceInfo);
+    console.log(centroid);
+    // Second Arc is for the Legend Inflexion Point
+
+    const inflexionInfo = {
+      innerRadius: radius + INFLEXION_PADDING,
+      outerRadius: radius + INFLEXION_PADDING,
+      startAngle: grp.startAngle,
+      endAngle: grp.endAngle,
+    };
+
+    const inflexionPoint = arcPathGenerator.centroid(inflexionInfo);
+
+    const isRightLabel = inflexionPoint[0] > 0;
+    const labelPosX = inflexionPoint[0] + 50 * (isRightLabel ? 1 : -1);
+    const textAnchor = isRightLabel ? "start" : "end";
+    const label = grp.data.source + " (" + grp.value + ")";
+
+    return (
+      <g key={i}>
+        <path d={slicePath} fill={colors[i]} />
+        <circle cx={centroid[0]} cy={centroid[1]} r={2} />
+        <line
+          x1={centroid[0]}
+          y1={centroid[1]}
+          x2={inflexionPoint[0]}
+          y2={inflexionPoint[1]}
+          stroke={"black"}
+          fill={"black"}
+        />
+        <line
+          x1={inflexionPoint[0]}
+          y1={inflexionPoint[1]}
+          x2={labelPosX}
+          y2={inflexionPoint[1]}
+          stroke={"black"}
+          fill={"black"}
+        />
+        <text
+          x={labelPosX + (isRightLabel ? 2 : -2)}
+          y={inflexionPoint[1]}
+          textAnchor={textAnchor}
+          dominantBaseline="middle"
+          fontSize={14}>
+          {label}
+        </text>
+      </g>
+    );
+  });
 
   return (
-    <div>
-      <svg width={width} height={height} style={{ display: "inline-block" }}>
-        <g transform={`translate(${width / 2}, ${height / 2})`}>
-          {arcs.map((arc, i) => {
-            return <path key={i} d={arc} fill={colors[i]} />;
-          })}
-        </g>
-      </svg>
-    </div>
+    <svg width={width} height={height} style={{ display: "inline-block" }}>
+      <g transform={`translate(${width / 2}, ${height / 2})`}>{shapes}</g>
+    </svg>
   );
+
+  // const centroid = arcPathGenerator.centroid(sliceInfo);
+  // const slicePath = arcPathGenerator(sliceInfo);
+
+  // const arcs = pie.map(
+  //   (p) =>
+  //     arcPathGenerator({
+  //       innerRadius: 0,
+  //       outerRadius: radius,
+  //       startAngle: p.startAngle,
+  //       endAngle: p.endAngle,
+  //     }) ?? undefined,
+  // );
+
+  // return (
+  //   <div>
+  //     <svg width={width} height={height} style={{ display: "inline-block" }}>
+  //       <g transform={`translate(${width / 2}, ${height / 2})`}>
+  //         {arcs.map((arc, i) => {
+  //           return <path key={i} d={arc} fill={colors[i]} />;
+  //         })}
+  //       </g>
+  //     </svg>
+  //   </div>
+  // );
 };
 
 // export default ResponsiveDonut;
